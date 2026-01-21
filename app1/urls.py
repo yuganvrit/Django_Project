0eb1e946ab7e74b063eb70c245f76564fc6d7f68
yuganvrit <yugan@vrittechnologies.com>
2026-01-21 13:57:35 +0545

    added new path

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 import app1.views as views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('info/', views.info, name='info'),
+
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    #we can pass dictionary as context to render
+    context = {
+        'name': 'Django',
+        'version': '1.11'
+    }
+    return render(request, 'index.html', context)
+
+
+
+def info(request):
+    #we can pass dictionary as context to render
+    context = {
+        'name': 'Yugan',
+        'age': '25'
+    }
+    return render(request, 'info.html', context)
